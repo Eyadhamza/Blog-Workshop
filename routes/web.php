9cd1e281bd74fe560974889ac5765f2d0dee25cd
Eyad Hamza <EyadHamza0@outlook.com>
2022-03-01 02:54:25 +0200

    Routes

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,62 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/home', function () {
+    return 'Hello World';
+})->name('home');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/categories', function () {
+    return 'Hello World';
+})->name('categories.index');
+
+Route::get('/categories/{category}', function () {
+    return 'Hello World';
+})->name('categories.show');
+
+Route::get('/articles', function () {
+    return 'Hello World';
+})->name('articles.index');
+
+Route::get('/articles/create', function () {
+    return 'Hello World';
+})->name('articles.create');
+
+
+Route::post('/articles', function () {
+    return 'Hello World';
+})->name('articles.store');
+
+Route::get('/articles/{article}', function () {
+    return 'Hello World';
+})->name('articles.show');
+
+Route::post('/articles/{article}/comments', function () {
+    return 'Hello World';
+})->name('comments.store');
+
+Route::get('/articles/{article}/edit', function () {
+    return 'Hello World';
+})->name('articles.edit');
+
+Route::patch('/articles/{article}', function () {
+    return 'Hello World';
+})->name('articles.update');
+
+Route::delete('/articles/{article}', function () {
+    return 'Hello World';
+})->name('articles.destroy');
+
+
+Route::get('/contact-us/show', function () {
+    return 'Hello World';
+})->name('contact-us.show');
+
+Route::post('/contact-us', function () {
+    return 'Hello World';
+})->name('contact-us.store');
+
+Route::post('/newsletter-subscribe', function () {
+    return 'Hello World';
+})->name('subscribe');
+
+
