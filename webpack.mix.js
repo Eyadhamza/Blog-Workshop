84b4f0d02ad7e87f87340ec7d406430fdbf20e55
Eyad Hamza <EyadHamza0@outlook.com>
2022-03-03 17:03:01 +0200

    integrating the template

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
- | Mix Asset Management
+ | Mix asset Management
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
